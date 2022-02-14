21b3632fb27d4d726269b98c89a434f2b99019e4
gurbanli97 <ali.e.gurbanli@gmail.com>
2022-02-14 11:56:11 +0400

    WIP:notifications

--- components/elements/Calendar.vue
+++ components/elements/Calendar.vue
@@ -3,7 +3,7 @@
     <FullCalendar :options="calendarOptions" :plugins="calendarOptions.plugins" ref="calendar">
       <template v-slot:eventContent='arg'>
 
-        <div class="tooltip-trigger" v-b-tooltip.hover :id="`tooltip-target-event-${arg.event.id}`">
+        <div class="fullcalendar-tooltip" v-b-tooltip.hover :id="`tooltip-target-event-${arg.event.id}`">
           <div class="fc-event-time">{{ arg.timeText }} </div>
           <div class="fc-event-title fc-sticky">{{ arg.event.title }}</div>
         </div>
@@ -33,16 +33,16 @@
       return {
         calendarOptions: {
           plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
-          height: 'auto',
+          // height: 'auto',
           // selectable: true,
           // editable:true,
           moreLinkContent:(args) => `+${args.num}`,
           handleWindowResize: true,
           windowResizeDelay: 0,
           initialView: 'dayGridMonth',
-          aspectRatio: 2.2,
+          aspectRatio: 2.5,
           eventClick: this.handleEventClick,
-          dayMaxEventRows: 3,
+          dayMaxEventRows: 4,
           eventTimeFormat: {
             hour12: false,
             hour: '2-digit',
@@ -51,35 +51,45 @@
           },
           //  eventBackgroundColor: '#black',
           eventDisplay: 'block',
-          events: [{
+          events: [
+            {
               id: 0,
               title: 'All Day Eventasdad',
               start: new Date(2022, 1, 3),
               backgroundColor: '#8840FB'
             },
-            {
+             {
               id: 1,
+              title: 'All Day Eventasdad',
+              start: new Date(2022, 1, 3),
+              backgroundColor: '#8840FB'
+            },
+            {
+              id: 2,
               title: 'All Day Eventasdasd',
               start: new Date(2022, 1, 3),
               backgroundColor: '#FFAA00'
             },
             {
+              id: 4,
               title: 'All Day Eventasdasd',
               start: new Date(2022, 1, 3),
               backgroundColor: '#00D68F'
             },
             {
+              id: 5,
               title: 'All Day Eventasdasd',
               start: new Date(2022, 1, 3),
               backgroundColor: '#3366FF'
             },
             {
+              id: 6,
               title: 'All Day Eventasdasd',
               start: new Date(2022, 1, 3),
               backgroundColor: '#FF3D71'
             },
             {
-              id: 4,
+              id: 7,
               title: 'All Day Eventasdasd',
               start: '2022-02-09 21:00',
               backgroundColor: '#FF3D71'
---
+++ pages/notifications/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="notifications">
+      <page-header :title="'Notifications'" :showActions="true"/>
+      <div class="container">
+          <div class="notifications-inner">
+              <div class="notifications-today">
+                  <span class="notification-date">Today</span>
+                      <div class="notification-card" 
+                  v-for="notification in notifications" 
+                  :key="notification.id"
+                  >
+                 <NuxtLink :to="`/notifications/${notification.id}`">
+
+                      <span class="unread-dot" v-if="!notification.read"></span>
+                      <div class="avatar">
+                          <img src="~images/qarpiz.jpg" alt="">
+                      </div>
+                      <div class="details">
+                          <div class="details-head">
+                              <strong>{{notification.username}}</strong>
+                              <span>{{notification.timestamp}}</span>
+                          </div>
+                          <div class="details-text">
+                              <p>{{notification.description}}</p>
+                          </div>
+                      </div>
+                 </NuxtLink>
+
+                  </div>
+              </div>
+              <div 
+              class="notifications-others"
+              v-for="notification in notifications" 
+              :key="notification.id"
+              >
+              <NuxtLink :to="`/notifications/${notification.id}`">
+                  <span class="notification-date">{{notification.date}}</span>
+                  <div class="notification-card">
+                      <span class="unread-dot" v-if="!notification.read"></span>
+                      <div class="avatar">
+                          <img src="~images/qarpiz.jpg" alt="">
+                      </div>
+                      <div class="details">
+                          <div class="details-head">
+                              <strong>{{notification.username}}</strong>
+                              <span>{{notification.timestamp}}</span>
+                          </div>
+                          <div class="details-text">
+                              <p>{{notification.description}}</p>
+                          </div>
+                      </div>
+                  </div>
+                  </NuxtLink>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+     head() {
+         return {
+             title: 'Notifications',
+         }
+  },
+    data() {
+        return {
+            notifications: [
+                {
+                    id:0,
+                    username: 'Orxan Anvarli',
+                    date: '09.02.2022',
+                    timestamp: '4:10 pm',
+                    description: 'Gravida nibh id egestas nibh proin. Eget justo tortor viverra pharetra laoreet consequat ac lorem egestas. In velit, vulputate ut pharetra nisl. Arcu eu egestas phasellus dignissim elit ac. Felis enim suspendisse aenean',
+                    read: false
+                },
+                {
+                    id:1,
+                    username: 'Orxan Anvarli',
+                    date: '09.02.2022',
+                    timestamp: '4:10 pm',
+                    description: 'Gravida nibh id egestas nibh proin. Eget justo tortor viverra pharetra laoreet consequat ac lorem egestas. In velit, vulputate ut pharetra nisl. Arcu eu egestas phasellus dignissim elit ac. Felis enim suspendisse aenean',
+                    read: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
