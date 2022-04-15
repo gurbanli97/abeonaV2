ab27b10279cdae4d432b8f84c0fbb1b68542f2b2
gurbanli97 <ali.e.gurbanli@gmail.com>
2022-04-15 16:49:31 +0400

    WIP: authentication

--- pages/login.vue
+++ pages/login.vue
@@ -5,21 +5,36 @@
             <div class="login-box_left">
                 <h2>Log in</h2>
                 <span>Welcome to Abeona</span>
-                <b-form>
+                <b-form  @submit.prevent="handleSubmit">
                     <b-form-group>
-                        <form-field :placeholder="'Enter'" :label="'Username'" />
+                        <form-field 
+                        :placeholder="'Enter'" 
+                        :label="'Username'"
+                         :is-invalid="$v.form.username.$error"
+                         v-model="form.username"
+                        >
+                         <b-form-invalid-feedback v-if="!$v.form.username.required">
+                         Username input cant be empty
+                        </b-form-invalid-feedback>
+                        </form-field>
+                       
                     </b-form-group>
-                    <b-form-group :label="'Password'" class="password-input">
-                        <span @mousedown="showPassword = !showPassword" @mouseup="showPassword = !showPassword" @mouseleave="showPassword = false">
+                    <b-form-group :label="'Password'" :class="['password-input',{'is-invalid':$v.form.password.$error}]">
+                        <span @mousedown="showPassword = !showPassword" @mouseup="showPassword = !showPassword" @mouseleave="showPassword = false" v-show="form.password">
                             <icon :name="showPassword ? 'eye-slash' : 'eye'" />
                         </span>
                         <b-form-input
                         autocomplete="off"
                         :type="passwordInputType"
                         :placeholder="'Enter'"
+                        :class="{'is-invalid':$v.form.password.$error}"
+                        v-model="form.password"
                         />
+                        <b-form-invalid-feedback v-if="!$v.form.password.required">
+                         Password input cant be empty
+                        </b-form-invalid-feedback>
                     </b-form-group>
-                    <button class="btn">Log in</button>
+                    <button class="btn" type="submit" >Log in</button>
                 </b-form>
             </div>
             <div class="login-box_right">
@@ -46,15 +61,52 @@
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators'
 export default {
+    layout: 'empty',
     data(){
         return {
-            showPassword: false
+            showPassword: false,
+            form:{
+                username: '',
+                password: ''
+            }
         }
+    },
+    validations:{
+        form:{
+            username: {required},
+            password: {required}
+        }
+
     },
     computed:{
         passwordInputType(){
             return this.showPassword ? 'text' : 'password'
+        }
+    },
+    methods:{
+        async handleSubmit(){
+            this.$v.form.$touch();
+
+            if (this.$v.$invalid)
+                return
+            
+            try{
+                this.$cookies.removeAll()
+                 await this.$auth.loginWith('local',{
+                    data: {
+                        password: this.form.password,
+                        username: this.form.username
+                    }
+                    
+                })
+
+            }catch(err){
+                this.$toast.error('Unable to log in with provided credentials')
+
+            }
+              
         }
     }
 }
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,12 @@
-export default function ({ $axios, error: nuxtError }) {
+export default function ({ $axios,store, error: nuxtError }) {
     $axios.onRequest(config => {
       config.headers['Content-Type'] = 'application/json';
-      config.headers['Authorization'] = 'Token 59a1e6bf9efe056cc908427a64aa02568ae91c2c'
     });
   
-    $axios.onError(error => {
-      nuxtError({
-        statusCode: (error.response && error.response.status) || 500,
-        message: error.message || 'Server error',
-      });
-    });
+    // $axios.onError(error => {
+    //   nuxtError({
+    //     statusCode: (error.response && error.response.status) || 500,
+    //     message: error.message || 'Server error',
+    //   });
+    // });
   }
